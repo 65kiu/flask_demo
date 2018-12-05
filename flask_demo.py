d68b5db363c489f0a9c82c7997be1283903cfb14
545735880@qq.com <86683231hh>
2018-12-05 22:07:37 +0800

    基本hello world、绑定多个url、指定默认值的动态url

--- flask_demo.py
+++ flask_demo.py
@@ -2,15 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# 最基本的入口
 @app.route('/')
-@app.route('/ass')
-def hello_world():
+def index():
     return '<h1>Hello World!</h1>'
 
-@app.route('/cc_owefeemgnt', defaults={'name': 'index'})
-@app.route('/cc_owefeemgnt/<name>')
-def cc_owefeemgnt(name):
+# 一个视图函数可以绑定多个URL
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello World!</h1>'
+
+# 动态URL，可以指定默认值
+@app.route('/greet', defaults={'name': 'index'})
+@app.route('/greet/<name>')
+def greet(name):
     return '<h1>Hello, %s!</h1>' % name
 
 
